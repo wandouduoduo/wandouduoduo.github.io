8f280e1eb0e413a914a99607b6adf46e5588659b
wandouduoduo <wandouduoduo@163.com>
2020-01-16 16:28:26 +0800

    Site updated: 2020-01-16 16:28:24

--- dist/music.js
+++ dist/music.js
@@ -15,11 +15,17 @@ const ap = new APlayer({
         url: 'https://sharefs.yun.kugou.com/202001151809/90e984e1761d4b0b2ae4acac7c09c0d0/G010/M02/11/0D/qoYBAFUPHm2AQpDiAEGgyuaBG10838.mp3',
         cover: 'http://p1.music.126.net/PJNV84mjt_mDXEkxtjzB4w==/18957779486268444.jpg?param=130y130',
       },
-    {
+      {
         name: '麻雀',
         artist: '李荣浩',
         url: 'https://sharefs.yun.kugou.com/202001151807/d1d2b9ba55bba79437e9eb779debb476/G179/M06/09/04/U4cBAF3npDGAUGH5AD24_UK8ED0597.mp3',
         cover: 'http://p2.music.126.net/TzlSVBiNtpRD2b7MT2Hi-w==/109951164527590793.jpg?param=130y130',
-      }  
+      },
+      {
+        name: '怀念青春',
+        artist: '刘刚',
+        url: 'https://sharefs.yun.kugou.com/202001161620/523454a8dd58b4190774581759a46d02/G013/M09/04/15/rYYBAFULeqyAUeKKAEFz6g_8Mco392.mp3',
+        cover: 'http://imge.kugou.com/stdmusic/150/20141211/20141211195510949852.jpg',
+      }
     ]
 });
