baba354170bb8ff2e3f830a910496f184c827cfa
wandouduoduo <wandouduoduo@163.com>
2020-02-07 11:19:41 +0800

    Site updated: 2020-02-07 11:19:39

--- dist/music.js
+++ dist/music.js
@@ -6,14 +6,14 @@ const ap = new APlayer({
 	   {
         name: "还有多少个十年",
         artist: '沈宁',
-        url: 'audio/还有多少个十年.mp3',
-        cover: 'images/还有多少个十年.jpg',
+        url: 'https://wandouduoduo.github.io/dist/audio/%E8%BF%98%E6%9C%89%E5%A4%9A%E5%B0%91%E4%B8%AA%E5%8D%81%E5%B9%B4.mp3',
+        cover: 'https://wandouduoduo.github.io/dist/images/%E8%BF%98%E6%9C%89%E5%A4%9A%E5%B0%91%E4%B8%AA%E5%8D%81%E5%B9%B4.jpg',
       },
       {
         name: '麻雀',
         artist: '李荣浩',
-        url: 'audio/麻雀.mp3',
-        cover: 'images/麻雀.jpg',
+        url: 'https://wandouduoduo.github.io/dist/audio/%E9%BA%BB%E9%9B%80.mp3',
+        cover: 'https://wandouduoduo.github.io/dist/images/%E9%BA%BB%E9%9B%80.jpg',
       }
     ]
 });
