633ccd37de85eb674ae33bc20c9a857342184fc3
wandouduoduo <wandouduoduo@163.com>
2020-02-07 11:08:26 +0800

    Site updated: 2020-02-07 11:08:24

--- dist/music.js
+++ dist/music.js
@@ -3,29 +3,17 @@ const ap = new APlayer({
     fixed: true,
     autoplay: false,
     audio: [
-	  {
+	   {
         name: "还有多少个十年",
         artist: '沈宁',
-        url: 'https://sharefs.yun.kugou.com/202001151822/00a386107bb3e5241828022fe8dbe9bf/G132/M02/05/19/ZJQEAFsOd6uANsw6AD2nhw2_pP8577.mp3',
-        cover: 'http://p2.music.126.net/W0iLDEeY8bjpYVcNT0Mr2g==/17787899114524329.jpg?param=130y130',
-      },
-	  {
-        name: '我们的时光',
-        artist: '赵雷',
-        url: 'https://sharefs.yun.kugou.com/202001151809/90e984e1761d4b0b2ae4acac7c09c0d0/G010/M02/11/0D/qoYBAFUPHm2AQpDiAEGgyuaBG10838.mp3',
-        cover: 'http://p1.music.126.net/PJNV84mjt_mDXEkxtjzB4w==/18957779486268444.jpg?param=130y130',
+        url: 'audio/还有多少个十年.mp3',
+        cover: 'images/还有多少个十年.jpg',
       },
       {
         name: '麻雀',
         artist: '李荣浩',
-        url: 'https://sharefs.yun.kugou.com/202001151807/d1d2b9ba55bba79437e9eb779debb476/G179/M06/09/04/U4cBAF3npDGAUGH5AD24_UK8ED0597.mp3',
-        cover: 'http://p2.music.126.net/TzlSVBiNtpRD2b7MT2Hi-w==/109951164527590793.jpg?param=130y130',
-      },
-      {
-        name: '怀念青春',
-        artist: '刘刚',
-        url: 'https://sharefs.yun.kugou.com/202001161620/523454a8dd58b4190774581759a46d02/G013/M09/04/15/rYYBAFULeqyAUeKKAEFz6g_8Mco392.mp3',
-        cover: 'http://imge.kugou.com/stdmusic/150/20141211/20141211195510949852.jpg',
+        url: 'audio/麻雀.mp3',
+        cover: 'images/麻雀.jpg',
       }
     ]
 });
