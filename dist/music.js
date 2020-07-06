b01711decad36d3a7b6f443114d2ff2707f1063c
wandouduoduo <wandouduoduo@163.com>
2020-07-06 10:53:11 +0800

    Site updated: 2020-07-06 10:53:09

--- dist/music.js
+++ dist/music.js
@@ -6,14 +6,20 @@ const ap = new APlayer({
 	   {
         name: "还有多少个十年",
         artist: '沈宁',
-        url: 'https://wandouduoduo.github.io/dist/audio/%E8%BF%98%E6%9C%89%E5%A4%9A%E5%B0%91%E4%B8%AA%E5%8D%81%E5%B9%B4.mp3',
-        cover: 'https://wandouduoduo.github.io/dist/images/%E8%BF%98%E6%9C%89%E5%A4%9A%E5%B0%91%E4%B8%AA%E5%8D%81%E5%B9%B4.jpg',
+        url: 'https://wandouduoduo.gitee.io/dist/audio/%E8%BF%98%E6%9C%89%E5%A4%9A%E5%B0%91%E4%B8%AA%E5%8D%81%E5%B9%B4.mp3',
+        cover: 'https://wandouduoduo.gitee.io/dist/images/%E8%BF%98%E6%9C%89%E5%A4%9A%E5%B0%91%E4%B8%AA%E5%8D%81%E5%B9%B4.jpg',
       },
       {
         name: '麻雀',
         artist: '李荣浩',
-        url: 'https://wandouduoduo.github.io/dist/audio/%E9%BA%BB%E9%9B%80.mp3',
-        cover: 'https://wandouduoduo.github.io/dist/images/%E9%BA%BB%E9%9B%80.jpg',
+        url: 'https://wandouduoduo.gitee.io/dist/audio/%E9%BA%BB%E9%9B%80.mp3',
+        cover: 'https://wandouduoduo.gitee.io/dist/images/%E9%BA%BB%E9%9B%80.jpg',
+      },
+      {
+        name: '我们的时光',
+        artist: '赵雷',
+        url: 'https://wandouduoduo.gitee.io/dist/audio/我们的时光.mp3',
+        cover: 'https://wandouduoduo.gitee.io/dist/images/我们的时光.jpg',
       }
     ]
 });
